1bf66854bf7227e5cd5a577bcb714ebd0e66e7fe
lbferreira <luckasborges2010@hotmail.com>
2024-01-08 16:06:29 -0600

    automatic version number

--- src/fastnanquantile/__init__.py
+++ src/fastnanquantile/__init__.py
@@ -1,1 +1,8 @@
+import importlib
+
 from .fastnanquantile import nanquantile
+
+try:
+    __version__ = importlib.metadata.version("fastnanquantile") or "unknown"
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
